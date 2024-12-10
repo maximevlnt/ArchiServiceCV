7fb5b8ae277d4b58a90cf1ee5fe38284d3ab5ce7
maximevlnt <maxime.vlnt@gmail.com>
2024-12-10 16:18:48 +0100

    initial commit

---
+++ Code/Authentification/src/main/java/fr/insa/server/config/Authentification/AuthentificationApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.server.config.Authentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationApplication.class, args);
+	}
+
+}
---
+++ Code/RequestManager/src/test/java/fr/insa/server/config/RequestManager/RequestManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.server.config.RequestManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
